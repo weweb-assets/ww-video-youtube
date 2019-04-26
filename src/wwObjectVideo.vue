23fc292399b2d9d20e253d522fa32bd60741ba7b
fbriou <fbriou@gmail.com>
2019-04-26 18:41:43 +0200

    add url selector for 4 provider

--- src/wwObjectVideo.vue
+++ src/wwObjectVideo.vue
@@ -8,14 +8,14 @@
             <video v-if="wwObject.content.data.provider == 'local' && wwAttrs.wwCategory == 'background'" class="ww-video-element ww-local-video ww-video-bg" autoplay="true" loop="true" preload="metadata" playsinline>
                 <source v-bind:src="wwObject.content.data.id +  '#t=0.1'" type="video/mp4">
             </video>
-            
+
             <video v-if="wwObject.content.data.provider == 'local' && wwAttrs.wwCategory != 'background'" class="ww-video-element ww-local-video" controlslist="nodownload" preload="metadata" playsinline v-bind:loop="wwObject.content.data.loop" v-bind:autoplay="wwObject.content.data.autoplay" v-bind:muted="wwObject.content.data.muted" v-bind:controls="wwObject.content.data.controls">
                 <source v-bind:src="wwObject.content.data.id +  '#t=0.1'" type="video/mp4">
             </video>
 
             <!-- YOUTUBE -->
             <iframe v-if="wwObject.content.data.provider == 'youtube' && wwAttrs.wwCategory == 'background'" class="ww-video-element ww-video-bg" v-bind:src="'//www.youtube.com/embed/' + wwObject.content.data.id + '?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&mute=1&playlist=' + wwObject.content.data.id " frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-            
+
             <iframe v-if="wwObject.content.data.provider == 'youtube' && wwAttrs.wwCategory != 'background'" class="ww-video-element" v-bind:src="'//www.youtube.com/embed/' + wwObject.content.data.id + '?rel=0' + 
 			(wwObject.content.data.loop ? '&loop=1&playlist=' + wwObject.content.data.id : '') + 
 			(wwObject.content.data.autoplay ? '&autoplay=1' : '') + 
@@ -25,7 +25,7 @@
 
             <!-- VIMEO -->
             <iframe v-if="wwObject.content.data.provider == 'vimeo' && wwAttrs.wwCategory == 'background'" class="ww-video-element ww-video-bg" v-bind:src="'//player.vimeo.com/video/' + wwObject.content.data.id + '?autoplay=1&loop=1&background=1' " frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-            
+
             <iframe v-if="wwObject.content.data.provider == 'vimeo' && wwAttrs.wwCategory != 'background'" class="ww-video-element" v-bind:src="'//player.vimeo.com/video/' + wwObject.content.data.id + '?a=0' + 
 			(wwObject.content.data.loop ? '&loop=1' : '') + 
 			(wwObject.content.data.autoplay ? '&autoplay=1' : '') + 
@@ -34,12 +34,12 @@
 
             <!-- DAILYMOTION -->
             <iframe v-if="wwObject.content.data.provider == 'dailymotion' && wwAttrs.wwCategory == 'background'" class="ww-video-element ww-video-bg" v-bind:src="'//www.dailymotion.com/embed/video/' + wwObject.content.data.id + '?autoplay=1&mute=1' " frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-            
+
             <iframe v-if="wwObject.content.data.provider == 'dailymotion' && wwAttrs.wwCategory != 'background'" class="ww-video-element" v-bind:src="'//www.dailymotion.com/embed/video/' + wwObject.content.data.id" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
 
             <!-- TWITCH -->
             <iframe v-if="wwObject.content.data.provider == 'twitch' && wwAttrs.wwCategory == 'background'" class="ww-video-element ww-video-bg" v-bind:src="'//player.twitch.tv/?channel=' + wwObject.content.data.id + '?autoplay=1&muted=1' " frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-            
+
             <iframe v-if="wwObject.content.data.provider == 'twitch' && wwAttrs.wwCategory != 'background'" class="ww-video-element" v-bind:src="'//player.twitch.tv/?channel=' + wwObject.content.data.id" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
         </div>
     </div>
@@ -250,7 +250,220 @@ export default {
                     ratio: 1920 / 1080
                 };
             }
-        }
+        },
+        getInfoFromUrl(url) {
+            const info = {};
+            if (url.indexOf('youtube.com') != -1) {
+                info.provider = 'youtube';
+                const temp = url.split('v=')[1];
+                info.id = temp.split('?')[0];
+            } else if (url.indexOf('vimeo.com') != -1) {
+                info.provider = 'vimeo';
+                const temp = url.split('m/')[1];
+                info.id = temp.split('?')[0];
+            } else if (url.indexOf('dailymotion.com') != -1) {
+                info.provider = 'dailymotion';
+                const temp = url.split('video/')[1];
+                info.id = temp.split('?')[0];
+            } else if (url.indexOf('twitch.tv') != -1) {
+                info.provider = 'twitch';
+                const temp = url.split('tv/')[1];
+                info.id = temp.split('?')[0];
+            }
+            console.log('INFO ', info)
+            return info;
+        },
+        /*=============================================m_ÔÔ_m=============================================\
+          EDIT VIDEO
+        \================================================================================================*/
+        async edit() {
+            wwLib.wwObjectHover.setLock(this);
+
+            wwLib.wwPopups.addStory('WWVIDEO_EDIT', {
+                title: {
+                    en: 'Edit slider',
+                    fr: 'Editer le slider'
+                },
+                type: 'wwPopupList',
+                buttons: null,
+                storyData: {
+                    list: {
+                        WWVIDEO_URL: {
+                            separator: {
+                                en: 'Style',
+                                fr: 'Style'
+                            },
+                            title: {
+                                en: 'URL',
+                                fr: 'URL'
+                            },
+                            desc: {
+                                en: 'Edit video source',
+                                fr: 'Éditer la source de la vidéo'
+                            },
+                            icon: 'wwi wwi-edit-planet',
+                            shortcut: 's',
+                            next: 'WWVIDEO_URL'
+                        },
+                        EDIT_STILE: {
+                            separator: {
+                                en: 'Style',
+                                fr: 'Style'
+                            },
+                            title: {
+                                en: 'Change slider style',
+                                fr: 'Changer l\'apparence du slider'
+                            },
+                            desc: {
+                                en: 'Borders, shadow, ...',
+                                fr: 'Bordures, ombres, ...'
+                            },
+                            icon: 'wwi wwi-edit-style',
+                            shortcut: 's',
+                            next: 'WWVIDEO_STYLE'
+                        },
+                        EDIT_RATIO: {
+                            title: {
+                                en: 'Change slider ratio',
+                                fr: 'Changer le ratio du slider'
+                            },
+                            desc: {
+                                en: 'Portrait, square, landscape, ...',
+                                fr: 'Portrait, carré, paysage, ...'
+                            },
+                            icon: 'wwi wwi-ratio',
+                            shortcut: 'r',
+                            next: 'WWVIDEO_RATIO'
+                        },
+                        EDIT_ANIM: {
+                            separator: {
+                                en: 'Interaction',
+                                fr: 'Interaction'
+                            },
+                            title: {
+                                en: 'Animation',
+                                fr: 'Animation'
+                            },
+                            desc: {
+                                en: 'Change animation',
+                                fr: 'Choisir l\'animation à l\'apparition de l\'image'
+                            },
+                            icon: 'wwi wwi-anim',
+                            shortcut: 'a',
+                            next: 'ANIMATION'
+                        },
+                        EDIT_CHANGE: {
+                            title: {
+                                en: 'Change object type',
+                                fr: 'Changer le type d\'objet'
+                            },
+                            icon: 'wwi wwi-switch',
+                            shortcut: 't',
+                            next: 'SELECT_WWOBJECT'
+                        },
+                    }
+                }
+            })
+            wwLib.wwPopups.addStory('WWVIDEO_RATIO', {
+                title: {
+                    en: 'Slider Ratio',
+                    fr: 'Ratio du slider'
+                },
+                type: 'wwPopupImageRatio',
+                buttons: {
+                    NEXT: {
+                        text: {
+                            en: 'Ok',
+                            fr: 'Valider'
+                        },
+                        next: false
+                    }
+                }
+            })
+            wwLib.wwPopups.addStory('WWVIDEO_STYLE', {
+                title: {
+                    en: 'Image style',
+                    fr: 'Style de l\'image'
+                },
+                type: 'wwPopupImageStyle',
+                buttons: {
+                    OK: {
+                        text: {
+                            en: 'Ok',
+                            fr: 'Valider'
+                        },
+                        next: false
+                    }
+                }
+            })
+
+            wwLib.wwPopups.addStory('WWVIDEO_URL', {
+                title: {
+                    'en': 'EDIT URL',
+                    'fr': 'EDITEZ l\'URL',
+                },
+                type: 'wwPopupForm',
+                storyData: {
+                    fields: [
+                        {
+                            label: {
+                                en: 'Video URL :',
+                                fr: 'URL de la vidéo :'
+                            },
+                            desc: {
+                                en: 'the address on top of your browser',
+                                fr: 'l\'adresse en haut de votre navigateur'
+                            },
+                            type: 'text',
+                            key: 'url',
+                            valueData: 'url'
+                        }
+                    ]
+                },
+                buttons: {
+                    NEXT: {
+                        text: {
+                            en: 'Ok',
+                            fr: 'Ok'
+                        },
+                        next: false
+                    }
+                }
+            });
+
+            let options = {
+                firstPage: 'WWVIDEO_EDIT',
+                data: {
+                    wwObject: this.wwObject
+                }
+            }
+
+            try {
+                const result = await wwLib.wwPopups.open(options);
+
+                console.log(result)
+
+                /*=============================================m_ÔÔ_m=============================================\
+                  STYLE
+                \================================================================================================*/
+                if (typeof (result.url) != 'undefined') {
+                    const info = this.getInfoFromUrl(result.url)
+                    if (info) {
+                        this.wwObject.content.data.id = info.id;
+                        this.wwObject.content.data.provider = info.provider
+                    }
+                }
+
+                this.wwObjectCtrl.update(this.wwObject);
+
+                this.wwObjectCtrl.globalEdit(result);
+
+            } catch (error) {
+                console.log(error);
+            }
+
+            wwLib.wwObjectHover.removeLock();
+        },
     },
     mounted() {
         this.init();
@@ -342,3 +555,5 @@ export default {
     }
 }
 </style>
+
+
